5525925f4cc0c700376aeaaff09f8a39c031833b
Divya <86043076+Divya4879@users.noreply.github.com>
2025-03-22 10:52:52 +0530

    Added a Confirmation Modal when user wants to delete a habit

---
+++ src/components/ConfirmationModal.tsx
@@ -0,0 +1,40 @@
+// src/components/ConfirmationModal.tsx
+import React from "react";
+import { Window } from "@progress/kendo-react-dialogs";
+
+interface ConfirmationModalProps {
+  message: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
+
+const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
+  message,
+  onConfirm,
+  onCancel,
+}) => {
+  return (
+    <Window
+      title="Confirm"
+      onClose={onCancel}
+      width={window.innerWidth * 0.4}
+      height={window.innerHeight * 0.4}
+    >
+      <div style={{ padding: "1rem", minWidth: "300px", margin:"auto" }}>
+        <p>{message}</p>
+        <div style={{ textAlign: "right" }}>
+          <button className="k-button" onClick={onCancel} style={{ marginRight: "0.5rem" }}>
+            Cancel
+          </button>
+          <button className="k-button k-primary" onClick={onConfirm}>
+            Confirm
+          </button>
+        </div>
+      </div>
+    </Window>
+  );
+};
+
+export default ConfirmationModal;
+
+
