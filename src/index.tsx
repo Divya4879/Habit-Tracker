e29bf88f713f34b3eff20ad26600001e9f3c03c5
Divya <86043076+Divya4879@users.noreply.github.com>
2025-03-22 11:11:25 +0530

    Added KendoUI Themebuilder custom CSS cdn link

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Import KendoReact default theme (adjust the path if needed)
+import "@progress/kendo-theme-default/dist/all.css";
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
