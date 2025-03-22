3363543aa22eab8847314f1c094edc473b04b73c
Divya <86043076+Divya4879@users.noreply.github.com>
2025-03-22 10:59:30 +0530

    Added a section to toggle the screen styles between light and dark mode

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,31 @@
+// src/components/ThemeToggle.tsx
+import React, { useState, useEffect } from "react";
+
+const ThemeToggle: React.FC = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleTheme = () => {
+    setDarkMode((prev) => !prev);
+  };
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+  return (
+    <button 
+    style={{
+        border:"none",
+        padding: "0.5em",
+        margin: "1em",
+        fontSize: "2em",
+        borderRadius:"50%"
+    }}
+    className="k-button" onClick={toggleTheme}>
+      {darkMode ? "☀️" : "🌚"}
+    </button>
+  );
+};
+
+export default ThemeToggle;
